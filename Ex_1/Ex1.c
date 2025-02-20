c32768dffd0de0ec245d5da7ce7acd0ea2cf81d5
Joao <joao.sabioni.t0126451@edu.atec.pt>
2025-02-20 09:56:49 +0000

    Elaboracao Do Exe 1

--- Ex_1/Ex1.c
+++ Ex_1/Ex1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(){
+    int num=0;
+
+        for(int i=0; i<10; i++){
+            num+=2;
+            printf(" num = %d, i = %d", num,i);
+            printf("\nAqui");
+        }
 
     printf("Hello world");
     return 0;
